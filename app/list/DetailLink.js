b4f8d3c310e56f5eff23c1dc48f9c771cf3c4227
lhs0698 <yohs0698@naver.com>
2023-04-23 17:48:18 +0900

    feat: useRoute를 이용한 client에서 페이지이동 테스트

--- app/list/DetailLink.js
+++ app/list/DetailLink.js
@@ -20,4 +20,3 @@ export default function DetailLink() {
 // router.forward() -> 앞으로 가기
 // router.refresh() -> 새로고침 : but 페이지를 처음부터 로드를 하는것이 아니라 이전에 변경된 부분만 새로고침 해준다 공식문서에서는 soft refresh라고 함
 // router.prefetch('/경로) -> /경로의 내용을 미리 로드 한다. 페이지 방문 시 매우 빠르게 이동 할 수 있다. 빠른 웹 페이지를 만들 때 유용함
-
--- app/list/page.js
+++ app/list/page.js
@@ -30,3 +30,8 @@ export default async function List() {
 
 // <Link href={'/detail/' + result[i]._id}>
 // dynamic route를 사용 /list페이지에서 글제목을 누르면 알맞은 상세페이지로 이동할 수 있게 하는 링크 코드
+
+
+// 1. 여러페이지를 만들려면 Dynamic route 
+// 2. 현재 URL이 뭔지 궁금하다면 useRoute
+// 3. 페이지 이동, prefetch 기능은 useRoute
